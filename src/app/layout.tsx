b08ddb4bf68a6f8206c2b9e0a7bbe3760b8f3e0c
gold <93087797+goldyea@users.noreply.github.com>
2025-08-02 00:17:23 -0400

    Updated tsconfig.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "@vercel/geist/font/sans";
+import { GeistMono } from "@vercel/geist/font/mono";
 import "./globals.css";
 import ClientBody from "./ClientBody";
 import Script from "next/script";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
         <Script
           crossOrigin="anonymous"
